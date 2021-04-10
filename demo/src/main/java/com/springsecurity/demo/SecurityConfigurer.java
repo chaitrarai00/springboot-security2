10187c951c053dd0708ba6d15817d62223477381
chaitrarai00 <32186609+chaitrarai00@users.noreply.github.com>
2021-04-10 16:39:39 +0530

    spring security deafult h2

---
+++ demo/src/main/java/com/springsecurity/demo/SecurityConfigurer.java
@@ -0,0 +1,53 @@
+package com.springsecurity.demo;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	DataSource dataSource;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		auth.jdbcAuthentication()
+		.dataSource(dataSource)
+		.withDefaultSchema()
+		.withUser(
+				User.withUsername("user")
+				.password("user")
+				.roles("user")
+				)
+		.withUser(
+				User.withUsername("admin")
+				.password("admin")
+				.roles("admin")
+				);
+	}
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http.authorizeRequests()
+		.antMatchers("/admin").hasRole("admin")
+		.antMatchers("/user").hasAnyRole("admin","rules")
+		.antMatchers("/").permitAll()
+		.and().formLogin();
+	}
+}
